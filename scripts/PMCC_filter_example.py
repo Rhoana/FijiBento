c9b4537b98e1b82dfc4721ebebad948bf1132fbf
Thouis (Ray) Jones <thouis@seas.harvard.edu>
2015-08-14 12:35:18 -0400

    better return values

--- scripts/PMCC_filter_example.py
+++ scripts/PMCC_filter_example.py
@@ -10,26 +10,38 @@ FAIL_PMCC_CURVATURE_TOO_HIGH = 2
 FAIL_PMCC_MAXRATIO_TOO_HIGH = 3
 FAIL_PMCC_NOT_LOCALIZED = 4
 
-def PMCC_match(image, template, min_correlation=0.6, maximal_curvature_ratio=10, maximal_ROD=0.9):
+# TODO: smoothing?
+
+def PMCC_match(image, template, min_correlation=0.6, maximal_curvature_ratio=10, maximal_ROD=0.9, output=[None]):
+    if output[0] is None:
+        output[0] = (np.empty([sz - tz + 1 for sz, tz in zip(image.shape, template.shape)],
+                              dtype=np.float32),
+                     np.empty([sz - tz + 1 for sz, tz in zip(image.shape, template.shape)],
+                              dtype=np.float32))
+
+    correlation_image = output[0][0]
+    maximum_image = output[0][1]
+
     # compute the correlation image
-    correlation_image = cv2.matchTemplate(image, template, cv2.TM_CCOEFF_NORMED)
+    cv2.matchTemplate(image, template, cv2.TM_CCOEFF_NORMED, correlation_image)
 
     # find local maxima
-    maxima_mask = (correlation_image == maximum_filter(correlation_image, size=3))
+    maximum_filter(correlation_image, size=3, output=maximum_image)
+    maxima_mask = (correlation_image == maximum_image)
     maxima_values = correlation_image[maxima_mask]
     maxima_values.sort()
 
     if maxima_values[-1] < min_correlation:
-        return None, (FAIL_PMCC_SCORE_TOO_LOW, maxima_values[-1])
+        return None, FAIL_PMCC_SCORE_TOO_LOW, maxima_values[-1]
 
     # TrakEM2 code uses (1 + 2nd_best) / (1 + best) for this test...?
     if (maxima_values.size > 1) and (maxima_values[-2] / maxima_values[-1] > maximal_ROD):
-        return None, FAIL_PMCC_MAXRATIO_TOO_HIGH
+        return None, FAIL_PMCC_MAXRATIO_TOO_HIGH, maxima_values[-2] / maxima_values[-1]
 
     # find the maximum location
     mi, mj = np.unravel_index(np.argmax(correlation_image), correlation_image.shape)
     if (mi == 0) or (mj == 0) or (mi == correlation_image.shape[0] - 1) or (mj == correlation_image.shape[1] - 1):
-        return None, FAIL_PMCC_ON_EDGE
+        return None, FAIL_PMCC_ON_EDGE, None
 
     # extract pixels around maximum
     [[c00, c01, c02],
@@ -46,7 +58,7 @@ def PMCC_match(image, template, min_correlation=0.6, maximal_curvature_ratio=10,
     det = dxx * dyy - dxy * dxy
     trace = dxx + dyy
     if (det <= 0) or (trace * trace / det > maximal_curvature_ratio):
-        return None, FAIL_PMCC_CURVATURE_TOO_HIGH
+        return None, FAIL_PMCC_CURVATURE_TOO_HIGH, trace * trace / det
 
     # localize by Taylor expansion
     # invert Hessian
@@ -59,24 +71,32 @@ def PMCC_match(image, template, min_correlation=0.6, maximal_curvature_ratio=10,
     oy = -ixy * dx - iyy * dy
 
     if abs(ox) >= 1 or abs(oy) >= 1:
-        return None, FAIL_PMCC_NOT_LOCALIZED
+        return None, FAIL_PMCC_NOT_LOCALIZED, (ox, oy)
 
-    return True, (mi + oy, mj + ox)
+    return True, (mi + oy, mj + ox), None
 
 if __name__ == '__main__':
     # template = cv2.imread(sys.argv[1], 0)  # flags=0 -> grayscale
     image_1 = 255 - cv2.imread(sys.argv[1], 0)
     image_2 = 255 - cv2.imread(sys.argv[2], 0)
 
-    for i in range(2):
+    rows, cols = image_2.shape
+    M = cv2.getRotationMatrix2D((cols / 2, rows / 2), -180 * np.pi * 0.006, 1)
+    image_2 = cv2.warpAffine(image_2, M, (cols,rows))
+    
+    for i in range(3):
         image_1 = cv2.resize(image_1, ((image_1.shape[0] / 2, image_1.shape[1] / 2)), interpolation=cv2.INTER_CUBIC)
         image_2 = cv2.resize(image_2, ((image_2.shape[0] / 2, image_2.shape[1] / 2)), interpolation=cv2.INTER_CUBIC)
 
-    for i in range(100, image_1.shape[0], 100):
-        for j in range(0, image_1.shape[1], 100):
-            template = image_1[i:, j:][:200, :200].copy()
-            result, location = PMCC_match(image_2, template, min_correlation=0.3)
+    print image_1.shape
+
+    for i in range(100, image_1.shape[0], 5):
+        for j in range(0, image_1.shape[1], 5):
+            template = image_1[i:, j:][:25, :25].copy()
+            result, location, value = PMCC_match(image_2, template, min_correlation=0.)
             if result:
-                print location
+                pass
+            #print location
             else:
-                print "     No match"
+                if location != 3:
+                    print "     No match", location, value
